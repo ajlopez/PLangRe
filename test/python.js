11ab2a10b2254923693eb37b14534c7f220ca268
ajlopez <webmaster@ajlopez.com>
2013-07-13 20:16:13 -0300

    python reserved words, some functions, using checker and triggers

---
+++ test/python.js
@@ -0,0 +1,38 @@
+
+var python = require('../lib/python'),
+    tokenizer = require('../lib/tokenizer'),
+    checker = require('../lib/checker'),
+    assert = require('assert');
+
+// test reserved words
+
+var text = "\
+False      class      finally    is         return \
+None       continue   for        lambda     try \
+True       def        from       nonlocal   while \
+and        del        global     not        with \
+as         elif       if         or         yield \
+assert     else       import     pass \
+break      except     in         raise \
+";
+
+var tokens = tokenizer.getTokens(text);
+
+for (var k = 0; k < tokens.length; k++)
+    if (tokens[k].word)
+        assert.equal(python.reservedWord(text, tokens, k, { }), 'python');
+
+// import module
+
+var result = checker.check("import module", python);
+assert.ok(result);
+assert.ok(result.python);
+assert.equal(result.python, 2);
+
+// import from module import
+
+var result = checker.check("from module import *", python);
+assert.ok(result);
+assert.ok(result.python);
+assert.equal(result.python, 3);
+
