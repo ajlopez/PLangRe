8be180c9eb8ca73fa5a2eb5a74ae81bbc30866ea
ajlopez <webmaster@ajlopez.com>
2013-07-14 19:33:59 -0300

    First python gist

---
+++ files/python/README.md
@@ -0,0 +1,5 @@
+# Python Files
+
+## Gists
+
+- [gist01.py](gist01.py) from https://gist.github.com/cx20/2902278
---
+++ test/pythonfiles.js
@@ -0,0 +1,14 @@
+
+var prangle = require('..'),
+    fs = require('fs'),
+    path = require('path'),
+    assert = require('assert');
+
+processFile('gist01.py');
+
+function processFile(filename) {
+    var text = fs.readFileSync(path.join(__dirname, '..', 'files', 'python', filename)).toString();
+    var result = prangle.analyze(text);
+
+    assert.ok(result);    assert.equal(result, 'python');
+}
