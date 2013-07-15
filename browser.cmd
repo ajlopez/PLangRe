b65a16584b5eb97219ac68645848850945a840eb
ajlopez <webmaster@ajlopez.com>
2013-07-15 10:44:07 -0300

    Refactor, functions.recognizeWords, used in javascript

--- browser.cmd
+++ browser.cmd
@@ -1,3 +1,3 @@
 
 mkdir js
-copy .\prologue.txt + lib\tokenizer.js + lib\functions.js + lib\checker.js + lib\ruby.js + lib\python.js + lib\javascript.js + lib\csharp.js + lib\plangre.js + .\epilogue.txt js\plangre.js
+copy .\prologue.txt + lib\tokenizer.js + lib\checker.js + lib\functions.js + lib\ruby.js + lib\python.js + lib\javascript.js + lib\csharp.js + lib\plangre.js + .\epilogue.txt js\plangre.js
