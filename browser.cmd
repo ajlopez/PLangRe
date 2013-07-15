827b5e2ce824dcab7daecb7721245bfcb3cddec7
ajlopez <webmaster@ajlopez.com>
2013-07-14 21:30:09 -0300

    Browser file; sample demo

---
+++ browser.cmd
@@ -0,0 +1,3 @@
+
+mkdir js
+copy .\prologue.txt + lib\tokenizer.js + lib\functions.js + lib\checker.js + lib\ruby.js + lib\python.js + lib\javascript.js + lib\csharp.js + lib\plangre.js + .\epilogue.txt js\plangre.js
